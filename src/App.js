c8d0586126e5861bfc19bbd4cf047a54e6c9a963
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 16:23:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import torso from './torso.png';
 import './App.css';
+import Button from './Button.js'
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
         <img src={torso} className="App-torso" alt="torso" />
       </header>
       <h3>반갑습니다<br>토르소포맨 홍대상수점 입니다</h3>
-      <button class="startBtn">셀프 체크인 시작하기</button>
-      <button class="helpMsg">도움이 필요하시면 <span class="underline">여기</span>를 눌러 직원을 호출해주세요.</button>
+      <Button class="startBtn">셀프 체크인 시작하기</Button>
+      <Button class="helpMsg">도움이 필요하시면 <span class="underline">여기</span>를 눌러 직원을 호출해주세요.</Button>
     </div>
   );
 }
