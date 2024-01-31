2a47fbd32b9ee3f40353410e317b881383840865
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 23:35:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ function App() {
         <div className="le-dv"></div>
         <div className="ri-dv"></div>
       </div>
-      <div className="mainBody">
-        <div className="menuList">
-            <div className="active">
-              <span className="step">1</span><span><span>예약확인</span></span>
-            </div>
-            <div className="">
-            <span className="step">2</span><span><span>모발체크</span></span>
-            </div>
-            <div className="">
-            <span className="step">3</span><span><span>입장대기</span></span>
-            </div>
-        </div>
-      </div>
+      
   );
 }
 
