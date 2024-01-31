f5b07a2d35b3c7fa6e12b81744cb668234589c56
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 22:50:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,28 +9,6 @@ function App() {
         <div className="le-dv"></div>
         <div className="ri-dv"></div>
       </div>
-      <div className="mainBody">
-        <div className="menuList">
-            <div className="active">
-              <span className="step">1</span><span><span>예약확인</span></span>
-            </div>
-            <div className="">
-            <span className="step">2</span><span><span>모발체크</span></span>
-            </div>
-            <div className="">
-            <span className="step">3</span><span><span>입장대기</span></span>
-            </div>
-        </div>
-        <div className="AppBody">
-          <div className="AppLogo">
-            <header className="App-header">
-              <img src={torso} className="App-torso" alt="torso" />
-            </header>
-          </div>
-          <h3>반갑습니다\n토르소포맨 홍대상수점 입니다</h3>
-          <Button label="셀프 체크인 시작하기" styleClass="startBtn" onClick="" disabled={false} />
-        </div>
-      </div>
   );
 }
 
