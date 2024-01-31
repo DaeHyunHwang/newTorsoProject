82a966db4e0c86b3d07fb08ad50476a8cfeae2f7
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 17:55:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ function App() {
           <div class="">
           <span class="step">3</span><span><span>입장대기</span></span>
           </div>
-        </div>
-      <header className="App-header">
-        <img src={torso} className="App-torso" alt="torso" />
-      </header>
-      <h3>반갑습니다\n토르소포맨 홍대상수점 입니다</h3>
-      <Button label="셀프 체크인 시작하기" styleClass="startBtn" onClick="" disabled={false} />
+      </div>
+      <div className="AppBody">
+        <header className="App-header">
+          <img src={torso} className="App-torso" alt="torso" />
+        </header>
+        <h3>반갑습니다\n토르소포맨 홍대상수점 입니다</h3>
+        <Button label="셀프 체크인 시작하기" styleClass="startBtn" onClick="" disabled={false} />
+      </div>
     </div>
   );
 }
