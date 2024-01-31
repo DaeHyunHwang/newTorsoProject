2b1129181772209d05b3a771098939703f31512c
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 16:16:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
         </div>
       <header className="App-header">
         <img src={torso} className="App-torso" alt="torso" />
-        <h3>반갑습니다<br>토르소포맨 홍대상수점 입니다</h3>
         
       </header>
     </div>
