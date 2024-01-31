653cb7b305c09645daae4181fc8cb3212e38f24f
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 22:37:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Button from "./Button.js";
 
 function App() {
   return (
-    <div className="App">
-      <div class="mainDiv">
-        <div class="le-dv"></div>
-        <div class="ri-dv"></div>
+      <div className="mainDiv">
+        <div className="le-dv"></div>
+        <div className="ri-dv"></div>
       </div>
-      <div class="menuList">
-          <div class="active">
-            <span class="step">1</span><span><span>예약확인</span></span>
+      <div className="menuList">
+          <div className="active">
+            <span className="step">1</span><span><span>예약확인</span></span>
           </div>
-          <div class="">
-          <span class="step">2</span><span><span>모발체크</span></span>
+          <div className="">
+          <span className="step">2</span><span><span>모발체크</span></span>
           </div>
-          <div class="">
-          <span class="step">3</span><span><span>입장대기</span></span>
+          <div className="">
+          <span className="step">3</span><span><span>입장대기</span></span>
           </div>
       </div>
       <div className="AppBody">
@@ -28,7 +27,6 @@ function App() {
         <h3>반갑습니다\n토르소포맨 홍대상수점 입니다</h3>
         <Button label="셀프 체크인 시작하기" styleClass="startBtn" onClick="" disabled={false} />
       </div>
-    </div>
   );
 }
 
