46a08b5c6c8845aa02f9bbccd7f006fd7f98289c
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 15:07:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="menuList">
+          <div class="active">
+            <span class="step">1</span><span><span>예약확인</span></span>
+          </div>
+          <div class="">
+          <span class="step">2</span><span><span>모발체크</span></span>
+          </div>
+          <div class="">
+          <span class="step">3</span><span><span>입장대기</span></span>
+          </div>
+        </div>
         <img src={torso} className="App-torso" alt="torso" />
       </header>
     </div>
