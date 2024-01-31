37cc47bff7a5ca06fe6b84470b3f9a5827af588f
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-01-31 16:32:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         </div>
       <header className="App-header">
         <img src={torso} className="App-torso" alt="torso" />
-        <h3>반갑습니다<br>토르소포맨 홍대상수점 입니다</h3>
+        
         <Button className="startBtn">셀프 체크인 시작하기</Button>
         <Button className="helpMsg">도움이 필요하시면 <span className="underline">여기</span>를 눌러 직원을 호출해주세요.</Button>
       </header>
