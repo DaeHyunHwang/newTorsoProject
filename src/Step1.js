558f5190630375b4a35050a13e6208dca6cc15d5
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-02-01 15:11:11 +0900

    Update Step1.js

--- src/Step1.js
+++ src/Step1.js
@@ -1,6 +1,9 @@
 import './App.css';
 import './styles.css';
-import torso from './torso.png';
+import master1 from './master1.png';
+import master2 from './master2.png';
+import master3 from './master3.png';
+import master4 from './master4.png';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -27,10 +30,10 @@ function Step1() {
           </div>
           <div className="masterTab">
             <ul className="masterList">
-              <li><img src={torso} className="" alt="torso" /><p><label>도하쌤</label></p></li>
-              <li><img src={torso} className="" alt="torso" /><p><label>민종쌤</label></p></li>
-              <li><img src={torso} className="" alt="torso" /><p><label>태원쌤</label></p></li>
-              <li><img src={torso} className="" alt="torso" /><p><label>진성쌤</label></p></li>
+              <li><img src={master1} className="" alt="master1" /><p><label>도하쌤</label></p></li>
+              <li><img src={master2} className="" alt="master2" /><p><label>민종쌤</label></p></li>
+              <li><img src={master3} className="" alt="master3" /><p><label>태원쌤</label></p></li>
+              <li><img src={master4} className="" alt="master4" /><p><label>진성쌤</label></p></li>
             </ul>
           </div>
         <div className="title"><h3>어떤 선생님으로 예약해주셨나요?</h3></div>
