d359ea239ed31b830f0e0f63d6267501e5bf2958
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-02-01 14:17:40 +0900

    Update Step1.js

--- src/Step1.js
+++ src/Step1.js
@@ -17,6 +17,13 @@ function Home(){
 function Step1() {
   return (
       <div className="mainBody">
+          <div className="tabWrap tab_typeL">
+            <ul className="tabList">
+              <li className="on">예약확인</li>
+              <li className="">모발체크</li>
+              <li className="">입장대기</li>
+            </ul>
+          </div>
           <div className="title"><h3>어떤 선생님으로 예약해주셨나요?</h3></div>
       </div>
   );
