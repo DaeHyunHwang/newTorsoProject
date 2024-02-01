2e3618be48de45c379433864dd295af0590077b9
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-02-01 13:47:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import torso from './torso.png';
 import './App.css';
 import './styles.css';
 import Button from "./Button.js";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-export default function App() {
-  
+function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-          <Routes>
-
-          </Routes>
-        </BrowserRouter>
-    </div>
+      <div className="mainBody">
+        <div className="tabWrap tab_typeL">
+            <ul className="tabList">
+              <li className="on">예약확인</li>
+              <li className="">모발체크</li>
+              <li className="">입장대기</li>
+            </ul>
+        </div>
+        <header className="App-header">
+          <img src={torso} className="App-torso" alt="torso" />
+        </header>
+        <h3>반갑습니다<p>토르소포맨 홍대상수점 입니다</p></h3>
+        <Button label="셀프 체크인 시작하기" styleClass="startBtn" onClick="startSelfCheck()" />
+        <a href="#" className="helpMsg">도움이 필요하시면 <span className="underline">여기</span>를 눌러 직원을 호출해주세요.</a>
+      </div>
   );
 }
+export default App;
