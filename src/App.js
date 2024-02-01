2c2ee260762b9e0c261c9438f531e984c8a11f41
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-02-01 13:50:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import torso from './torso.png';
 import './App.css';
 import './styles.css';
 import Button from "./Button.js";
+
+function startSelfCheck(){
+  alert("다음 페이지 이동예정");
+}
+
 function App() {
   return (
       <div className="mainBody">
