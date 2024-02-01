28ebac857e889ddd5423dda7f19143e51c5cba1c
DaeHyunHwang <hwangdh@a2m.co.kr>
2024-02-01 13:43:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './styles.css';
 import Button from "./Button.js";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './home.js';
+import Home from './home';
 
 export default function App() {
   
